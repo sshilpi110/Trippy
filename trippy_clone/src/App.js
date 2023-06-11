862232c5eead910d1a3cc97580a1106ca0dc9c72
shilpi <105913922+sshilpi110@users.noreply.github.com>
2023-06-11 17:50:46 +0530

    hamburger

---
+++ trippy_clone/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./Component/Navbar"
+import './App.css';
+import {Routes} from "react-router-dom"
+function App() {
+  return (
+    <div className="App">
+     {/* <h1>Travel & tourist</h1> */}
+     
+     <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ trippy_clone/src/Component/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import { Component } from "react"
+import { Link } from "react-router-dom"
+import { MenuItems } from "./MenuItems"
+import "./NavbarStyle.css"
+class Navbar extends Component {
+    state = { clicked: false }
+    handleClick = () => {
+      this.setState({clicked: !this.state.clicked})
+    }
+    render() {
+
+        return (
+            <nav className="navbarItem">
+                <h1 className="navbar-logo">Trippy</h1>
+
+                <div className="Menu-icons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas-fa-bars"}></i>
+                </div>
+
+                <ul className="nav-menu">
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href="/"><i className={item.icon}></i>{item.title}</a>
+                            </li>
+                        )
+                    })}
+                    <button>SignUp</button>
+                </ul>
+            </nav>
+        )
+    }
+}
+export default Navbar;
